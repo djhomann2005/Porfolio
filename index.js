9d9a93caa43bd66e27142d121ff8823866466929
DJ Homann <djhomann2005@gmail.com>
2025-02-16 19:55:41 -0600

    Added mouseover functionality to projects.

---
+++ index.js
@@ -0,0 +1,9 @@
+function showDescription(projectElement) {
+    const description = projectElement.querySelector(".desc-container");
+    description.style.display = "block";
+}
+
+function hideDescription(projectElement) {
+    const description = projectElement.querySelector(".desc-container");
+    description.style.display = "none";
+}
